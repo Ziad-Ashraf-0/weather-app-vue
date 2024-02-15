8210f01af424c00576318874d9a9375b39ab98c8
Ziad Ashraf <zashraf011@gmail.com>
2024-02-15 18:33:58 +0200

    done

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,10 +6,24 @@
 
       <ul class="absoult bg-weather-secondary text-white w-full shadow-md py-2 px-1 top-[66px]"
         v-if="mapboxSearchResults">
-        <li v-for="searchResult in mapboxSearchResults" :key="searchResult.id" class="py-2 cursor-pointer">
-          {{ searchResult.place_name }}
-        </li>
+        <p v-if="searchError"> Sorry, something went wrong, pls try again.</p>
+        <p v-if="!searchError && mapboxSearchResults.length === 0"> No results match your query, try a diffrent term</p>
+        <template v-else>
+          <li v-for="searchResult in mapboxSearchResults" :key="searchResult.id" class="py-2 cursor-pointer"
+            @click="previewCity(searchResult)">
+            {{ searchResult.place_name }}
+          </li>
+        </template>
+
       </ul>
+    </div>
+    <div class="flex flex-col gap-4">
+      <Suspense>
+        <CityList />
+        <template #fallback>
+          <CityCardSkeleton />
+        </template>
+      </Suspense>
     </div>
   </main>
 </template>
@@ -18,19 +32,47 @@
 <script setup>
 import { ref } from "vue"
 import axios from "axios";
+import { useRouter } from "vue-router"
+import CityList from "../components/CityList.vue"
+import CityCardSkeleton from "@/components/CityCardSkeleton.vue";
+
+const router = useRouter();
+
+
+const previewCity = (searchResult) => {
+  console.log(searchResult);
+  const [city, state] = searchResult.place_name.split(",");
+  router.push({
+    name: 'cityview',
+    params: { state: state.replaceAll(" ", ""), city: city },
+    query: {
+      lat: searchResult.geometry.coordinates[1],
+      lng: searchResult.geometry.coordinates[0],
+      preview: true,
+    }
+  })
+
+}
 
 const mapboxAPIKey = "pk.eyJ1IjoiemlhZDQyIiwiYSI6ImNsbDJpeDk1djAwcmIzcnBrMWN3ZWhwcGoifQ.xnmZsIW5Y-GkUEJs74KzbA"
 const searchQuery = ref("")
 const queryTimeout = ref(null)
 const mapboxSearchResults = ref(null);
+const searchError = ref(null);
 
 const getSearchResults = () => {
   clearTimeout(queryTimeout.value);
   queryTimeout.value = setTimeout(async () => {
     if (searchQuery.value !== '') {
-      const result = await axios.get(`https://api.mapbox.com/geocoding/v5/mapbox.places/${searchQuery.value}.json?access_token=${mapboxAPIKey}&types=place`)
-      mapboxSearchResults.value = result.data.features;
+      try {
+        const result = await axios.get(`https://api.mapbox.com/geocoding/v5/mapbox.places/${searchQuery.value}.json?access_token=${mapboxAPIKey}&types=place`)
+        mapboxSearchResults.value = result.data.features;
+
+      } catch (e) {
+        searchError.value = true;
+      }
       return;
+
     }
     mapboxSearchResults.value = null;
   }, 300)
